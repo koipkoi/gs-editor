034bf49e2e6c1add4e4e512d5083a8fff4ff8cf3
koipkoi <hrh6914@gmail.com>
2022-10-17 11:54:48 +0900

    Update text_convert.go
    - 버퍼 기반 인코딩 함수 추가
    - 성능 개선

--- pokegold/utils/text_convert.go
+++ pokegold/utils/text_convert.go
@@ -11,7 +11,8 @@ var (
 	//go:embed charmap.txt
 	charmap string
 
-	charmapTable = map[int]string{}
+	charmapTable        = map[int]string{}
+	charmapReverseTable = map[string]int{}
 )
 
 func init() {
@@ -24,6 +25,7 @@ func init() {
 
 			if err == nil {
 				charmapTable[int(key)] = value
+				charmapReverseTable[value] = int(key)
 			}
 		}
 	}
@@ -61,36 +63,55 @@ func TextEncode(str string) []byte {
 //
 // 인코딩된 바이트와 성공 유무를 함께 반환함
 func TryTextEncode(str string) ([]byte, bool) {
-	var bytes []byte
-	str = strings.ReplaceAll(str, "\r\n", "\n")
+	bytes := make([]byte, 4096)
+	if length, ok := TryTextEncodeBuffered(bytes, str); ok {
+		return bytes[0:length], true
+	}
+	return nil, false
+}
 
-	for i := 0; i < len(str); {
-		seek := 0
+// 문자열을 전용 인코딩 바이트로 치환하여 버퍼에 기록
+func TextEncodeBuffered(buffer []byte, str string) int {
+	length, _ := TryTextEncodeBuffered(buffer, str)
+	return length
+}
+
+// 문자열을 전용 인코딩 바이트로 치환하여 버퍼에 기록
+//
+// 인코딩된 바이트의 길이와 성공 유무를 함께 반환함
+func TryTextEncodeBuffered(buffer []byte, str string) (int, bool) {
+	str = strings.ReplaceAll(str, "\r\n", "\n")
+	strLength := len(str)
+	bufferIndex := 0
+
+	var foundKeys []int
+	seek := 0
+	for i := 0; i < strLength; {
+		seek = 0
 
 		for j := 0; j < 32; j++ {
 			seek++
 
-			var foundKeys []int
-			if i+j >= len(str) {
-				return nil, false
+			foundKeys = nil
+			if i+j >= strLength {
+				return -1, false
 			}
 
 			char := str[i : i+j+1]
-			for key, value := range charmapTable {
-				if char == value {
-					foundKeys = append(foundKeys, key)
-					continue
-				}
+			if value, ok := charmapReverseTable[char]; ok {
+				foundKeys = append(foundKeys, value)
 			}
 
 			// 등록된 문자 존재
 			if len(foundKeys) == 1 {
 				n := foundKeys[0]
 				if n > 0xff {
-					bytes = append(bytes, byte(((n >> 8) & 0xff)))
-					bytes = append(bytes, byte(n&0xff))
+					buffer[bufferIndex] = byte(((n >> 8) & 0xff))
+					buffer[bufferIndex+1] = byte(n & 0xff)
+					bufferIndex += 2
 				} else {
-					bytes = append(bytes, byte(n))
+					buffer[bufferIndex] = byte(n)
+					bufferIndex++
 				}
 				break
 			}
@@ -99,10 +120,11 @@ func TryTextEncode(str string) ([]byte, bool) {
 			if strings.HasPrefix(char, "[") && strings.HasSuffix(char, "]") {
 				numberStr := char[1 : len(char)-1]
 				if i, err := strconv.ParseInt(numberStr, 16, 8); err == nil {
-					bytes = append(bytes, byte(i))
+					buffer[bufferIndex] = byte(i)
+					bufferIndex++
 					break
 				} else {
-					return nil, false
+					return -1, false
 				}
 			}
 		}
@@ -110,5 +132,5 @@ func TryTextEncode(str string) ([]byte, bool) {
 		i += seek
 	}
 
-	return bytes, true
+	return bufferIndex, true
 }
